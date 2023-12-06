b15e0ba33abc50abd8b37bb0db90087b7a08de19
Jenny Puranen <124900133+puranenjenny@users.noreply.github.com>
2023-12-06 18:04:38 +0200

    sulje sovellus scripti alkuun ja loppuun

---
+++ Assets/Scripts/SuljeSovellusScripti.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuljeSovellusScripti : MonoBehaviour
+{
+    public void SuljeSovellus()// sulkee sovelluksen
+    {
+        Application.Quit();
+    }
+}
