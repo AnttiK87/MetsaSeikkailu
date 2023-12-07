31bc581a300311703e8634b71e91794537a5d0c3
Jenny Puranen <124900133+puranenjenny@users.noreply.github.com>
2023-12-07 15:25:54 +0200

    pientä viilausta alkuun ja loppuun ja infonapin scripti loppuun

--- Assets/Scripts/Scene1/InfoNappi.cs
+++ Assets/Scripts/Scene1/InfoNappi.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InfoNappi : MonoBehaviour
 {
     public GameObject infoNappi; // info nappi
     public GameObject infoPanel; // info paneli
     public GameObject infoNappiCanvas; // info nappi canvas
-    void Awake() // ei tuhoa tätä scriptiä kun vaihdetaan sceneä
+
+    void Awake() // ei tuhoa tätä scriptiä tai info objekteja kun vaihdetaan sceneä
     {
         DontDestroyOnLoad(gameObject);
         DontDestroyOnLoad(infoNappi);
@@ -15,29 +17,34 @@ public class InfoNappi : MonoBehaviour
         DontDestroyOnLoad(infoNappiCanvas);
 
     }
-    // Start is called before the first frame update
-    void Start()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) // kun scene vaihtuu
     {
-        
+        if (scene.buildIndex == 7) // tarkistetaan onko loaded scene 7 jolloin tuhotaan info objektit ja tämä scripti
+        {
+            Destroy(infoNappi);
+            Destroy(infoPanel);
+            Destroy(infoNappiCanvas);
+            Destroy(gameObject); // viimeisenä tuhotaan tämä scripti
+
+            SceneManager.sceneLoaded -= OnSceneLoaded; // poistetaan eventti
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy() // kun tämä scripti tuhotaan niin poistetaan myös eventti
     {
-        
+        SceneManager.sceneLoaded -= OnSceneLoaded; 
     }
 
     //kun painetaan info nappia niin info paneli tulee näkyviin ja kun painetaan uudestaan se menee pois näkyvistä
-
     public void InfoNappiPainettu()
     {
-        if (!infoPanel.activeSelf && infoPanel != null)
+        if (!infoPanel.activeSelf && infoPanel != null) // jos info panel ei ole aktiivinen ja se ei ole null
         {
-            infoPanel.SetActive(true);
+            infoPanel.SetActive(true); // laitetaan info panel aktiiviseksi
         }
-        else
+        else // muuten
         {
-            infoPanel.SetActive(false);
+            infoPanel.SetActive(false); // laitetaan info panel ei aktiiviseksi
         }
     }
 }
