dd4b3ca346b5dc0116459673948927cc2f5d0af7
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2023-12-12 15:54:53 +0200

    Timerin aloituksesta häivytys pois ja muutos

--- Assets/Scripts/SceneAloitus/AloitaPeliJaTimer.cs
+++ Assets/Scripts/SceneAloitus/AloitaPeliJaTimer.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class AloitaPeliJaTimer : MonoBehaviour
 {
-    public GameObject haivytysEkaan;
-    public float Haivytysaika = 3f;
     public void StartGameAndTimer() // aloittaa pelin ja ajastimen
     {
 
@@ -20,18 +18,5 @@ public class AloitaPeliJaTimer : MonoBehaviour
         {
             print("PelinKestoScripti not found"); // jos pelin kesto scriptiä ei löydy annetaan error
         }
-
-        StartCoroutine(HaivytysKutsu(1)); // loadataan scene 1 häivytykseen tarvittavalla viiveellä ja peli alkaa
-    }
-
-    //asetetaan häivytys aktiiviseksi ja annetaan viive seuraavan kentän lataamiselle
-    IEnumerator HaivytysKutsu(int levelIndex)
-    {
-        haivytysEkaan.SetActive(true);
-
-        yield return new WaitForSeconds(Haivytysaika);
-
-        // Haetaan nykyisen kentän numero
-        SceneManager.LoadScene(levelIndex);
     }
 }
