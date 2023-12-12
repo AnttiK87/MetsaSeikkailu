9f388960f21a4190360616f937ffcb81f94e2d31
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2023-12-12 14:33:42 +0200

    timerin sammutus

--- Assets/Scripts/Scene6/LopetaPelinAjastin.cs
+++ Assets/Scripts/Scene6/LopetaPelinAjastin.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class LopetaPelinAjastin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void LopetaPeliJaTimer() // kun painetaan nappia joka lopettaa pelin
+    public void LopetaPeliJaTimer() // Kutsutaan pelin lopettavan animaation lopuksi
     {
         PelinKestoScripti ajastin = FindObjectOfType<PelinKestoScripti>(); // etsitään pelin kesto scripti
         if (ajastin != null) // jos löytyy
         {
             ajastin.PysaytaAjastin(); // pysäytetään ajastin
         }
-       // lataa scene 7
-       UnityEngine.SceneManagement.SceneManager.LoadScene(7);
     }
 }
