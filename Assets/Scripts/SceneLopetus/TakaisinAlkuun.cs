8a7b6ea831b426aa9b9a2abce13a058f60cb2fa0
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2023-12-12 16:08:42 +0200

    Häivytyksiä

--- Assets/Scripts/SceneLopetus/TakaisinAlkuun.cs
+++ Assets/Scripts/SceneLopetus/TakaisinAlkuun.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class TakaisinAlkuun : MonoBehaviour
 {
+    public Animator Haivytys;
+
+    public float Haivytysaika = 3f;
     public void Takaisin() // kun painetaan nappia palataan alkunäyttöön
     {
         PelinKestoScripti ajastin = FindObjectOfType<PelinKestoScripti>(); // etsitään pelin kesto scripti
@@ -12,6 +15,16 @@ public class TakaisinAlkuun : MonoBehaviour
         {
             ajastin.NollaaAjastin(); // nollataan ajastin
         }
-        SceneManager.LoadScene(0);
+        StartCoroutine(HaivytysKutsu(0));
+    }
+
+    IEnumerator HaivytysKutsu(int levelIndex)
+    {
+        Haivytys.SetTrigger("Alku");
+
+        yield return new WaitForSeconds(Haivytysaika);
+
+        // Haetaan nykyisen kentän numero
+        SceneManager.LoadScene(levelIndex);
     }
 }
